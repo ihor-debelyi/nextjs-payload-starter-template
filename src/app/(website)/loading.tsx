fd7f8986cf2f538e3f5a6c82bb46d3994132b18c
Ihor Debelyi <ihor.debelyi@gmail.com>
2024-11-18 22:06:03 +0100

    updated plugins

--- src/app/(website)/loading.tsx
+++ src/app/(website)/loading.tsx
@@ -1,13 +1,7 @@
 import { CircularProgress } from "@nextui-org/progress";
 
 const Loading = () => {
-  return (
-    <CircularProgress
-      className="mx-auto"
-      classNames={{ svg: "w-24 h-24 drop-shadow-md" }}
-      aria-label="Loading page ..."
-    />
-  );
+  return <CircularProgress className="mx-auto" classNames={{ svg: "w-24 h-24 drop-shadow-md" }} aria-label="Loading page ..." />;
 };
 
 export default Loading;
--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -1,6 +1,5 @@
 import { Button } from "@nextui-org/button";
 
 export default async function Home() {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
   return <Button>Home</Button>;
 }
